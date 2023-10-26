44262edbf67f0d2e8550e30976ef9f249c027664
nitxir <149069003+nitxir@users.noreply.github.com>
2023-10-26 17:51:02 +0530

    divider.v
    design

--- divider.v
+++ divider.v
@@ -1,60 +1,55 @@
-
-module divider(  
-   input  clk,  
-   input  reset,  
-   input  start,  
-   input  [31:0]  A,  
-   input  [31:0]  B,  
-   output  [31:0]  D,  
-   output  [31:0]  R,  
-   output  ok ,   // =1 when ready to get the result   
-   output  err  
-   );  
-   reg       active;   // True if the divider is running  
-   reg [4:0]    cycle;   // Number of cycles to go  
-   reg [31:0]   result;   // Begin with A, end with D  
-   reg [31:0]   denom;   // B  
-   reg [31:0]   work;    // Running R  
-   // Calculate the current digit  
-   wire [32:0]   sub = { work[30:0], result[31] } - denom;  
-       assign err = !B;  
-   // Send the results to our master  
-   assign D = result;  
-   assign R = work;  
-   assign ok = ~active;
-   // The state machine  
-   always @(posedge clk,posedge reset) begin  
-     if (reset) begin  
-       active <= 0;  
-       cycle <= 0;  
-       result <= 0;  
-       denom <= 0;  
-       work <= 0;  
-     end  
-     else if(start) begin  
-       if (active) begin  
-         // Run an iteration of the divide.  
-         if (sub[32] == 0) begin  
-           work <= sub[31:0];  
-           result <= {result[30:0], 1'b1};  
-         end  
-         else begin  
-           work <= {work[30:0], result[31]};  
-           result <= {result[30:0], 1'b0};  
-         end  
-         if (cycle == 0) begin  
-           active <= 0;  
-         end  
-         cycle <= cycle - 5'd1;  
-       end  
-       else begin  
-         // Set up for an unsigned divide.  
-         cycle <= 5'd31;  
-         result <= A;  
-         denom <= B;  
-         work <= 32'b0;  
-         active <= 1;  
-       end  
-     end  
-   end  
- endmodule  
+module divider(
+input clk,
+input reset,
+input start,
+input [7:0] A,
+input [7:0] B,
+output [7:0] D,
+output [7:0] R,
+output ok, // =1 when ready to get result
+output err
+);
+reg active; // true if the divider is running
+reg[4:0] cyc; // number of cycles to go
+reg[7:0] res; // begin with A, end with D
+reg[7:0] den; // B
+reg[7:0] work; // running R
+// calculate the current digit
+wire[7:0] sub = { work[6:0], res[7] } -den;
+assign err = !B;
+// send the results to master
+assign D = res;
+assign R = work;
+assign ok = ~active;
+//state machine
+always @(posedge clk , posedge reset) begin
+if (reset) begin
+active <= 0;
+cyc <= 0;
+res <= 0;
+den <= 0;
+end
+else if(start) begin
+if(active) begin
+//run iteration of divide
+if(sub[7] == 0) begin
+work <=sub[7:0];
+res <= {res[6:0],1'b1};
+end
+else begin
+work <= { work[6:0],res[7]};
+res <= {res[6:0],1'b0};
+end
+cyc <= cyc - 5'd1;
+end
+else begin
+//set up for an unsigned divide
+cyc <= 5'd31;
+res <= A;
+den <= B;
+work <= 8'b0;
+active <= 1;
+end
+end
+end
+endmodule
